b959c7db5ca9f33c7db83b5abe3d6b9c170ea800
felipe98 <angelfelipe1398@gmail.com>
2020-06-22 19:55:49 -0500

    Hotfix url base de datos

--- config/config.js
+++ config/config.js
@@ -7,4 +7,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB = 'mongodb://localhost:27017/cafe';
 
-(process.env.NODE_ENV === 'dev') ? process.env.URLDB = urlDB : process.env.MONGO_URL; 
+(process.env.NODE_ENV === 'dev') ? process.env.URLDB = urlDB 
+                                : process.env.URLDB = process.env.MONGO_URL; 
